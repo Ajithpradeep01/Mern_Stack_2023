05d08776ec172e17b34bc5fa2093e37d92f2712d
Ajithpradeep01 <ajithachu2k18@gmail.com>
2023-12-12 11:54:51 +0530

    created sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async(req,res,next)=>{
 
-    const { username,email,password }=req.body
-    const hashedPassword = bcryptjs.hashSync(password,10)
-    const newUser = new User({ username,email,password: hashedPassword })
-    console.log(hashedPassword)
+    const { username, email, password }=req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username,email,password:
+         hashedPassword })
+         console.log(hashedPassword)
+         console.log(newUser)
     try {
         await newUser.save()
         res.status(201).json("user created succesfully")
@@ -14,4 +18,20 @@ export const signup = async(req,res,next)=>{
         next(error)
     }
  
+}
+
+export const signin = async (req,res,next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404,'User Not Found'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+    console.log(password)
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials'))
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
+       const {password: pass, ...rest} =validUser._doc
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+    }catch (error){
+        next(error)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -26,8 +26,8 @@ app.use('/api/auth',authRouter)
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal Server Error'
+    console.log(message)
     return res.status(statusCode).json({
-
         success:false,
         statusCode,
         message,
