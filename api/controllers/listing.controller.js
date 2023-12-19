fd87774b92e0242b1c358b7bb14860136a8e6ac0
Ajithpradeep01 <ajithachu2k18@gmail.com>
2023-12-19 22:57:12 +0530

    completed all functionalities of listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -11,13 +11,13 @@ export const createListing = async (req,res,next) => {
     }
 }
 
-export const deleteListing = async (req,res,next)=>{
+export const deleteListing = async (req,res,next) => {
     const listing = await Listing.findById(req.params.id)
 
     if(!listing){
         return next(errorHandler(404,'Listing not found!'))
     }
-    if(req.params.id !== listing.userRef){
+    if(req.user.id !== listing.userRef){
         return next(errorHandler(401,'You can only delete your own listings!'))
     }
     try {
